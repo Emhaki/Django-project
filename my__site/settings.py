302992bcc4e1dbd5ee0e91b42be209823465a671
Myeonghak Lee <mhmh779@naver.com>
2022-10-02 22:48:54 +0900

    CSS

--- my__site/settings.py
+++ my__site/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join("static"),)
 
 
 # Default primary key field type
